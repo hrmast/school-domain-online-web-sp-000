09024471ca93bad15a1f45f54992f2b859d71acd
Heath Mast <hrmast@gmail.com>
2020-05-20 03:47:51 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,24 +1,6 @@
 class School
   def initialized(name)
     @name = name
-    @roster = {}
-  end
-
-  def roster
-    @roster
-  end
-
-  def add_student
-
-
-  end
-
-  def grade
-
-  end
-
-  def sort
-
   end
 
 
