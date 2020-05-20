b369ee5ac2bd355888965f16d404f1d52a961e0a
Heath Mast <hrmast@gmail.com>
2020-05-20 03:30:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,15 +1,9 @@
 class School
-  attr_accessor :school, :roster, :grade, :add_student
-
-  NEW = []
 
   def initialized(name)
     @name = name
     @roster = {}
   end
 
-  def new=(new)
-    @new = new
-    NEW << new
-  end
+
 end
